1d06cff763a3d6977d7596127fa019b1cc2fa530
v-ntv <vincentv.ntv@gmail.com>
2025-06-16 19:31:41 +0200

    changement de l'index et donc du script js aussi

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
         en: {
             title: "Portfolio VERHUST Vincent",
             about: "About",
+            project: "Projects",
             aboutText: "With experience in IT and systems & networks, I have skills in web development as well as database management and systems and network administration. <br><br> Thanks to my work-study experiences, I have acquired a methodical approach to solving technical problems and server administration. I have also worked on projects such as setting up NGINX servers, monitoring and securing network infrastructures. <br><br> Passionate about development and new technologies, I am looking to join a company as a full stack developer, where I can continue to develop my skills and contribute effectively to ongoing projects.",
             skills: "Skills",
             contact: "Contact",
@@ -42,6 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
         fr: {
             title: "Portfolio VERHUST Vincent",
             about: "À propos",
+            project: "Projets",
             aboutText: " VOIR FICHIER INDEX.HTML POUR LES MODIF a faire sur les projets académiques/projets Fort d’une expérience en IT et en systèmes & réseaux, je possède des compétences en développement web ainsi qu’en gestion de bases de données et en administration systèmes et réseaux. <br><br> Grâce à mes expériences en alternance, j’ai acquis une approche méthodique dans la résolution de problèmes techniques et l’administration de serveurs. J’ai également travaillé sur des projets tels que la mise en place de serveurs NGINX, la supervision et la sécurisation d’infrastructures réseaux. <br><br> Passionné par le développement et les nouvelles technologies, je cherche à intégrer une entreprise en tant que développeur full stack, où je pourrai continuer à monter en compétences et contribuer efficacement aux projets en cours.",
             skills: "Compétences",
             contact: "Contact",
