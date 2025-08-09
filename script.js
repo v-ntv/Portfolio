cbb63720f8ef1fc4b5ee39d4dc2d754555a0b041
v-ntv <vincentv.ntv@gmail.com>
2025-08-09 16:12:35 +0200

    maj index

--- script.js
+++ script.js
@@ -18,10 +18,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const translations = {
         en: {
             title: "Portfolio VERHUST Vincent",
-            about: "About",
+            about: "Bio",
             project: "Projects",
-            aboutText: "With experience in IT and systems & networks, I have skills in web development as well as database management and systems and network administration. <br><br> Thanks to my work-study experiences, I have acquired a methodical approach to solving technical problems and server administration. I have also worked on projects such as setting up NGINX servers, monitoring and securing network infrastructures. <br><br> Passionate about development and new technologies, I am looking to join a company as a full stack developer, where I can continue to develop my skills and contribute effectively to ongoing projects.",
-            skills: "Skills",
+            aboutText: `Hi, I’m Vincent.
+<br><br>
+I am a student at Le Wagon in the Data Analyst bootcamp.
+<br><br>
+Before this, I worked in IT as a technician, where I handled customer support and server administration.
+<br><br>
+I am organized, rigorous and also i am someone who is calm under pressure as i was a security agent before switching to IT.
+<br><br>
+I decided to upskill with data analytics because I believe data is at the heart of every company and it’s a powerful asset for business decision.
+<br><br>
+My main strength is my background in IT, it gives me a solid foundation to understand the complexity of infrastructures, systems and computer languages and therefore analyze data effectively.`,
             contact: "Contact",
             email: "Your email:",
             message: "Your message:",
@@ -30,11 +39,19 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         fr: {
             title: "Portfolio VERHUST Vincent",
-            about: "À propos",
+            about: "Bio",
             project: "Projets",
-            aboutText: " VOIR FICHIER INDEX.HTML POUR LES MODIF a faire sur les projets académiques/projets Fort d’une expérience en IT et en systèmes & réseaux, je possède des compétences en développement web ainsi qu’en gestion de bases de données et en administration systèmes et réseaux. <br><br> Grâce à mes expériences en alternance, j’ai acquis une approche méthodique dans la résolution de problèmes techniques et l’administration de serveurs. J’ai également travaillé sur des projets tels que la mise en place de serveurs NGINX, la supervision et la sécurisation d’infrastructures réseaux. <br><br> Passionné par le développement et les nouvelles technologies, je cherche à intégrer une entreprise en tant que développeur full stack, où je pourrai continuer à monter en compétences et contribuer efficacement aux projets en cours.",
-            skills: "Compétences",
-            contact: "Contact",
+            aboutText: `Salut c'est Vincent.
+<br><br>
+Je suis étudiant au Wagon en formation Data Analyst.
+<br><br>
+Avant de commencer cette formation, j'ai travaillé dans l'informatique en tant que technicien systèmes et réseaux, où je m'occupais du support client et de l'administration des serveurs.
+<br><br>
+Je suis organisé, rigoureux et je sais aussi garder mon calme sous la pression comme je me suis formé en tant qu'agent de sécurité avant de me reconvertir dans l'informatique.
+<br><br>
+J'ai décidé de monter en compétences avec l'analyse de données car je suis convaincu que les données sont au cœur de toute entreprise et qu'elles constituent un atout majeur pour la prise de décision.
+<br><br>
+Mon principal atout réside dans mon expérience en informatique. Elle me permet de comprendre la complexité des infrastructures, des systèmes et des langages informatiques, et donc d'analyser efficacement les données.`,
             email: "Votre email :",
             message: "Votre message :",
             send: "Envoyer",
